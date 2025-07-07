3bc7ba683f60be5499ce3de09b9693f4ca99a641
JRamch <ramcj7742@gmail.com>
2025-07-07 14:25:23 -0400

    init

---
+++ insert.php
@@ -0,0 +1,14 @@
+<?php
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+try {
+$conn = new mysqli("localhost", "username", "password", "cp476");
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+$conn->set_charset("utf8mb4");
+} catch(Exception $e) {
+error_log($e->getMessage());
+exit('Error connecting to database');
+}
+$conn->close();
+?>
