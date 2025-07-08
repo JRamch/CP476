3cde6fa0102235298a6d466643fee3f9ce4b932b
JRamch <ramcj7742@gmail.com>
2025-07-08 12:39:48 -0400

    Reads Product.txt

--- insert.php
+++ insert.php
@@ -1,15 +1,28 @@
 <?php
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 try {
-$conn = new mysqli("localhost", "root", "Admin", "cp476");
-if ($conn->connect_error) {
-die("Connection failed: " . $conn->connect_error);
-}
-$conn->set_charset("utf8mb4");
+    //change 'root' and 'Admin' to your mysql username / password
+    $conn = new mysqli("localhost", "root", "Admin", "cp476"); 
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $conn->set_charset("utf8mb4");
 } catch(Exception $e) {
-error_log($e->getMessage());
-exit('Error connecting to database');
+    error_log($e->getMessage());
+    exit('Error connecting to database');
 }
+
+try{
+    $file = fopen("Product.txt", 'r');
+    $test = fgetcsv($file, 1000, ',','"',"");
+    for ($i = 0; $i < count($test); $i++){
+        echo $test[$i];
+    }
+    fclose($file);
+} catch(Exception $e){
+    exit("error opening Product.txt");
+}
+
 $conn->close();
-echo "success!";
+echo "\nsuccess!";
 ?>
